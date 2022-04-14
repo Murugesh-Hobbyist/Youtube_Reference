2a2fed6c9e3f2d0b92781d0d272528242af04e3e
Murugesh <murugesh.rcpilot@gmail.com>
2022-04-14 15:02:26 +0530

    added

---
+++ LASER/RF_Switching/Laser_Alarm/Laser_Alarm.ino
@@ -0,0 +1,20 @@
+int Sensor_Pin = A0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(Sensor_Pin, INPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() 
+{      
+    int Sensor_Value = analogRead(Sensor_Pin);
+    Serial.println(Sensor_Value);
+  
+    if (Sensor_Value < 30)
+       {digitalWrite(8, HIGH);
+       delay(5000);}
+    if (Sensor_Value <30) 
+       {digitalWrite(8, LOW);}
+}
